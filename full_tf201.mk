cda2eb4122a6f825c2abb31179884b88b85be410
Voyager2k <clego22284@aol.com>
2013-07-22 21:39:31 -0700

    Use XHDPI when MDPI is unavailable
    Change-Id: Idfe56b683134dd816de45c0d4490524457fa4ffd

--- full_tf201.mk
+++ full_tf201.mk
@@ -27,6 +27,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from toro device
 $(call inherit-product, device/asus/tf201/device_tf201.mk)
 
+# Use xhdpi drawables where appropriate, but prefer mdpi.
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 
