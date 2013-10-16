b7d5ecbd2c15da66d582d4b4d4c0df8ae8c38ef4
Voyager2k <clego22284@aol.com>
2013-10-16 16:56:26 -0700

    Tweaks for CarbonROM

--- carbon.mk
+++ carbon.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, device/asus/tf201/full_tf201.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/carbon/config/common_full_tablet_wifionly.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_tf201
-PRODUCT_BRAND := asus
+PRODUCT_NAME := carbon_tf201
+PRODUCT_BRAND := Asus
 PRODUCT_DEVICE := tf201
-PRODUCT_MODEL := EPAD
-PRODUCT_MANUFACTURER := asus
+PRODUCT_MODEL := Transformer Prime
+PRODUCT_MANUFACTURER := Asus
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.1.1/JRO03C/US_epad-10.4.2.18-20121122:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.1.1 JRO03C US_epad-10.4.2.18-20121122 release-keys"
 
 # Release name and versioning
